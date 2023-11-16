5d0f568b0ce6e76f3730df99736c7139731b293d
vedant jangid <vedjangid341@gmail.com>
2023-11-17 00:48:23 +0530

    added the PATCH and DELETE API routes

---
+++ components/ui/api-alert.tsx
@@ -0,0 +1,42 @@
+import { Server } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "./alert";
+import { Badge, BadgeProps } from "./badge";
+
+interface ApiAlertProps {
+    title: string;
+    description: string;
+    variant: "public" | "admin";
+}
+
+const textMap: Record<ApiAlertProps["variant"], string> = {
+    public: "Public",
+    admin: "Admin"
+}
+
+const variantMap: Record<ApiAlertProps["variant"], BadgeProps["variant"]> = {
+    public: "secondary",
+    admin: "destructive"
+}
+
+export const ApiAlert: React.FC<ApiAlertProps> = ({
+    title,
+    description,
+    variant = "public"
+}) => {
+    return (
+        <Alert>
+            <Server className="h-4 w-4" />
+            <AlertTitle className="flex items-center gap-x-2">
+                {title}
+                <Badge variant={variantMap[variant]}>
+                    {textMap[variant]}
+                </Badge>
+            </AlertTitle>
+            <AlertDescription>
+                <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+                    {description}
+                </code>
+            </AlertDescription>
+        </Alert>
+    )
+}
