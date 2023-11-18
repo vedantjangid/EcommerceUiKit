f334978d49d7a3166e01207fa16bb0e24465734f
vedant jangid <vedjangid341@gmail.com>
2023-11-18 22:29:41 +0530

    done for the day added tables for dashboard overview

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -6,9 +6,12 @@ import { Separator } from "@/components/ui/separator"
 import { Billboard } from "@prisma/client"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { BillboardColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
 interface BillboardClientProps {
-    data: Billboard[]
+    data: BillboardColumn[]
 }
 
 export const BillboardClient: React.FC<BillboardClientProps> = ({
@@ -29,6 +32,10 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({
                 </Button>
             </div>
             <Separator />
+            <DataTable searchKey='label' columns={columns} data={data} />
+            <Heading title="API" description="Api routes for billboards" />
+            <Separator />
+            <ApiList entityName="billboards" entityIdName="billboardId" />
         </>
     )
 }
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,5 +1,7 @@
+import { format } from 'date-fns'
 import prismadb from "@/lib/prismadb";
 import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
 
 
 const Billboards = async ({ params }: { params: { storeId: string } }) => {
@@ -13,11 +15,18 @@ const Billboards = async ({ params }: { params: { storeId: string } }) => {
         }
     });
 
+
+    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+        id: item.id,
+        label: item.label,
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
+    }))
+
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
 
-                <BillboardClient data={billboards} />
+                <BillboardClient data={formattedBillboards} />
             </div>
         </div>);
 }
