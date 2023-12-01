db25f233a4b26adcbbdc1cc1f17fd3756f1bd315
vedant jangid <vedjangid341@gmail.com>
2023-12-02 00:38:55 +0530

    almost done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -18,15 +18,32 @@ const Navbar = async () => {
     })
 
     return (
-        <div className="border-b">
-            <div className="flex h-16 items-center px-4">
+        // <div className="border-b">
+        //     <div className="flex h-16 items-center px-4">
+        //         <StoreSwitcher items={stores} />
+        //         <MainNav className="mx-6" />
+        //         <div className="ml-auto flex items-center space-x-4">
+        //             <UserButton afterSignOutUrl="/" />
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="border-b bg-white">
+            <div className="container mx-auto flex h-16 items-center px-4">
+                {/* Logo or Store Name */}
+                <h1 className="text-3xl font-light text-gray-800 mr-4">Essor</h1>
+
+                {/* Main Navigation */}
+                <MainNav className="ml-auto mx-6" />
+                {/* Store Switcher */}
                 <StoreSwitcher items={stores} />
-                <MainNav className="mx-6" />
+                {/* User Button */}
                 <div className="ml-auto flex items-center space-x-4">
                     <UserButton afterSignOutUrl="/" />
                 </div>
             </div>
         </div>
+
+
     );
 }
 
